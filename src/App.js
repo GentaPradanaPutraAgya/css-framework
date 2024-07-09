46615b7aaaff9b76f5f7fea0ad3c999c4fc912be
GentaPradanaPutraAgya <92007199+GentaPradanaPutraAgya@users.noreply.github.com>
2024-07-09 22:50:25 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App container">
+    <h1 className="text-danger">BootStrap</h1>
+      <button className="btn btn-primary">Submit</button>
+  </div>
+  );
+}
+
+export default App;
